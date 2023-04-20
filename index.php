bc1f93106f58e0e96f66b9fdc7633f740beccc56
Facundo Farias <facundo@saas.group>
2023-04-20 09:13:52 -0300

    Test change

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use Cowsayphp\Farm;
 
 header('Content-Type: text/plain');
 
-$text = "Set a message by adding ?message=<message here> to the URL (v2)";
+$text = "Set a message by adding ?message=<message here> to the URL (TEST)";
 if(isset($_GET['message']) && $_GET['message'] != '') {
 	$text = htmlspecialchars($_GET['message']);
 }
